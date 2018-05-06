9e29e90ab148dbe4331fe8513e23fb0a85736305
david landau <thelandaus@gmail.com>
2018-05-06 21:45:23 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,12 @@
-def fizzbuzz
+def fizzbuzz(int)
   if int % 3 == 0
     "Fizz"
-  end
   elsif int % 5 == 0
     "Buzz"
-  elsif
+  elsif int % 3 == 0 && int % 5 == 0
+    "FizzBuzz"
+  else 
+    nil
+  end
 end
 
